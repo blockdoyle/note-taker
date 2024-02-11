50407d43a11ac50446b1fb3d300bd8fb543bdc15
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-02-11 13:27:24 -0500

    Add data parsing and API route for retrieving notes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 // Set up the Express app
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// GET route for /api/notes returns the db.json file
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+);
 
 // GET route for /notes returns notes.html
 app.get('/notes', (req, res) =>
