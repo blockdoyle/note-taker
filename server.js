65dbffbc4ec863da6a130b3cc4830fa68d6481c3
Brock Lockhart-Doyle <blockdoyle@gmail.com>
2024-02-11 12:37:01 -0500

    Add routes for /notes and default route

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// GET route for /notes returns notes.html
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// If no matching route is found default to home
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
